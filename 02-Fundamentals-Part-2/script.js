b52f0b3e9247062f2ad39493303203a33d237bbf
poisonivy-dev <melodyiram09@gmail.com>
2022-09-25 22:42:49 +0500

    New Changes

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+//-------------Functions----------------------
+
+//function declaration -- can  use them even if the function is called before initialization
+function calculateAge(birthYear) {
+  return 2022 - birthYear;
+}
+
+//function expression -- anonymous functions -- cannot use them before they are initialized
+const age = function (birthYear) {
+  return 2022 - birthYear;
+};
+
+//Arrow Functions
+const calcAge = (birthYear) => 2022 - birthYear;
+
+const calculateRetirement = (birthYear, firstName) => {
+  const age = 2022 - birthYear;
+  const retirement = 65 - age;
+  return `${firstName} will retire in ${retirement} years`;
+};
+
+console.log(
+  calculateAge(1999),
+  age(1999),
+  calcAge(1999),
+  calculateRetirement(1999, "John")
+);
